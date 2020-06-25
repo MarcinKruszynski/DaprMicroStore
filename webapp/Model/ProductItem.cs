6d87409050163471ce4c66e63c55578afa7c91ba
MarcinKruszynski <martin.kruszynski@gmail.com>
2020-06-26 01:22:18 +0200

    products + webapp

---
+++ webapp/Model/ProductItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace webapp.Model
+{
+    public class ProductItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }        
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }        
+    }
+}
