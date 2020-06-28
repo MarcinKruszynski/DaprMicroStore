224881084d7bdca1b712984910d7009583b15dbf
MarcinKruszynski <martin.kruszynski@gmail.com>
2020-06-29 00:18:36 +0200

    add bookings prev

---
+++ bookings/Controllers/BookingsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using bookings.Model;
+using Dapr.Client;
+
+namespace bookings.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookingsController : ControllerBase
+    {
+        [Route("checkout")]
+        public async Task Checkout(BookingCheckout bookingCheckout, [FromServices] DaprClient dapr, [FromServices] ILogger<BookingsController> logger)
+        {
+
+        }
+    }
+}
