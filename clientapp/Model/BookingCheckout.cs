b9a5f00dfa16009bbacefd1d4408ab6061c8a8d4
MarcinKruszynski <martin.kruszynski@gmail.com>
2020-07-13 11:40:38 +0200

    add tye ingress + clientapp improvements

---
+++ clientapp/Model/BookingCheckout.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace clientapp.Model
+{
+    public class BookingCheckout
+    {
+        public string RequestId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
